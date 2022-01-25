0d91f8c1c36213d4d74db0ac2191aed6e70cad77
10kareem10 <karimelihab6@gmail.com>
2022-01-25 21:05:31 +0200

    5.styled comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import react from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 
 function App() {
   return (
+  
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
       
     </div>
