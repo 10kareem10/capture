cc6c782e432af1cd5f91846e4caef769322734b5
10kareem10 <karimelihab6@gmail.com>
2022-01-26 13:05:00 +0200

    8.creating Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react from "react";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 import GlobalStyle from "./components/GlobalStyle";
 
 
@@ -8,6 +9,7 @@ function App() {
   
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
       
     </div>
