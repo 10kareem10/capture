551f9ce7dd01bdd3d5259d3ef9487cb639d6f4cc
10kareem10 <karimelihab6@gmail.com>
2022-01-28 19:59:32 +0200

    11.aboutus animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,27 +3,17 @@ import home1 from "../img/home1.png";
 import styled from "styled-components";
 import { About,Hide,Image,Description } from "./styles";
 import { motion } from "framer-motion";
+import { titleAnim,fade,photoAnim } from "../animation";
 
 
 const AboutSection=()=>{
 
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: {
-          opacity: 1,
-          transition: { duration: 0.75 },
-        },
-      };
-
-      const container={
-          hidden:{x:100},
-          show:{x:0 , transition:{duration:0.75 , ease:"easeOut", staggerChildren:0.35}},
-      };
+    
     
     return(
         <About>
             <Description>
-                <motion.div variants={container} initial="hidden" animate="show" className="title">
+                <motion.div>
                     <Hide>
                     <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
@@ -34,15 +24,14 @@ const AboutSection=()=>{
                         <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography
+                <motion.p variants={fade}>Contact us for any photography
                  or videography ideas that you have, 
                  we have professionals with amazing skills.
-                 </p>
-                 <button>Contact Us</button>
+                 </motion.p>
+                 <motion.button variants={fade}>Contact Us</motion.button>
             </ Description>
             <Image>
-                <img src={home1} alt="guy with a camera"></img>
-
+                <motion.img variants={photoAnim} src={home1} alt="guy with a camera"/>
             </Image>
         </About>
 
